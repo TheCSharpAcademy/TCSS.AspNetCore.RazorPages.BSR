cdc5fa8a82c9828cc0be3fdb64434f86d137d94c
Pablo <thecsharpacademy@gmail.com>
2024-01-15 14:32:48 +1000

    Simple list of homes

---
+++ BSR/Models/Home.cs
@@ -0,0 +1,10 @@
+﻿namespace BSR;
+
+public class Home
+{
+    public int Id { get; set; }
+    public string Address { get; set; }
+    public decimal Price { get; set; }  
+    public int Area { get; set; }
+    public string ImageUrl { get; set; }
+}
