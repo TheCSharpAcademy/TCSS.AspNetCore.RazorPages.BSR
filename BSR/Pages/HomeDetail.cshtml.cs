29e19247dfa7bbe5338534f947769a52e449f1fb
Pablo <thecsharpacademy@gmail.com>
2024-01-26 08:38:52 +1000

    Removing delete logic?

--- BSR/Pages/HomeDetail.cshtml.cs
+++ BSR/Pages/HomeDetail.cshtml.cs
@@ -25,11 +25,4 @@ public class HomeDetailModel : PageModel
     {
         return _homeService.GetHomeById(id);
     }
-
-    public IActionResult OnPost(int id)
-    {
-        _homeService.DeleteHome(id);
-
-        return new OkResult();
-    }
 }
