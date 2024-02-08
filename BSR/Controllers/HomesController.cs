d4372bdbf47c5b13583d56d061669099ed0ab115
Pablo <thecsharpacademy@gmail.com>
2024-02-08 16:45:01 +1000

    Refactored add home

--- BSR/Controllers/HomesController.cs
+++ BSR/Controllers/HomesController.cs
@@ -7,7 +7,6 @@ namespace BSR.Controllers;
 public class HomesController: Controller
 {
     private readonly HomeService _homeService;
-    public List<Home> Homes { get; private set; }
 
     public HomesController(HomeService homeService)
     {
@@ -27,5 +26,32 @@ public class HomesController: Controller
         }
 
         return View(homesViewModel);
+    }
+
+    [HttpGet]
+    public IActionResult AddHomeView()
+    {
+        return View(); 
+    }
+
+    [HttpPost]
+    public IActionResult AddHome(Home newHome)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View("AddHomeView", newHome); 
+        }
+
+        try
+        {
+            _homeService.AddHome(newHome);
+            TempData["SuccessMessage"] = "Home added successfully!";
+            return RedirectToAction("Index", "Homes"); 
+        }
+        catch (Exception ex)
+        {
+            TempData["ErrorMessage"] = $"Error adding home: {ex.Message}";
+            return View("AddHomeView", newHome); 
+        }
     }
 }
